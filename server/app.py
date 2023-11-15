e568c31152811255085c3ec913699c481cf39fbb
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-14 18:39:30 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = True
 
 migrate = Migrate(app, db)
@@ -16,12 +16,36 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(plants, 200)
+
+    def post(self):
+        plant = Plant(**request.json)
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 201)
+
+
+api.add_resource(Plants, "/plants")
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = db.session.get(Plant, id)
+        # plant = Plant.query.get(id)
+        # plant = Plant.query.filter_by(id=id).first()
+        # plant = Plant.query.get_or_404(id)
+        try:
+            return make_response(plant.to_dict(), 200)
+        except AttributeError:
+            return make_response({"error": "Not found"}, 404)
 
-if __name__ == '__main__':
+
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
